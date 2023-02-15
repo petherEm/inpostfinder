8f5bf3d2cff81901693ace32db266e2c36983a5f
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2023-02-15 09:13:11 +0100

    component update

--- app/map/page.js
+++ app/map/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import MapAll from "../../components/mapAll";
+import MapAll from "../../components/MapAll";
 
 const Map = () => {
   const [details, setDetails] = useState([]);
